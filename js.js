18b3d7698fd9597a2b5df9a62057efcecf0782ed
p4c18op <149690192+p4c18op@users.noreply.github.com>
2024-06-13 20:13:09 +0300

    js.js

--- js.js
+++ js.js
@@ -1,12 +1,25 @@
-const js = (x, n) => {
-    let result = x;
+function getTypeOfNumbers() {
+    const arr = [0, null, NaN, 0, 1, 2, 4, "str"]
 
-    for (let i = 1; i < n; i++) {
-        result *= x;
+    let even = 0,
+        odd = 0,
+        zero = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] === 'number' && !isNaN(arr[i])) {
+            if (arr[i] === 0) {
+                zero += 1;
+            } else if (arr[i] % 2 === 0) {
+                even += 1;
+            } else {
+                odd += 1;
+            }
+        }
     }
 
-    return result;
+    console.log('Чётных чисел: ', even)
+    console.log('Нечётных чисел: ', odd)
+    console.log('Нулей: ', zero)
 }
 
-const result = js(10, 2)
-console.log(result)
+getTypeOfNumbers()
